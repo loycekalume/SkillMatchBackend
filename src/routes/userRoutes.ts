86f28193beb4999c16db748825e6c638d1eb4e26
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-04-21 11:14:27 +0300

    trying change1

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,11 +3,11 @@
 
 import express from 'express'
 import {  deleteUser, getUsers, getUserById, putUser } from '../controllers/userController'
-import { protect, requireRole } from '../middlewares/auth/protect';
+
 
 
 const router = express.Router()
-// router.use(protect); 
+
 
 router.get("/",getUsers),
 router.put("/",putUser ),
